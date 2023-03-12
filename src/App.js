b202cfe4a2a2c10990d01d99213acf2a8e25812b
strangeal <bajerijustice@gmail.com>
2023-03-12 11:14:37 -0700

    Modify routes for home and about component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Nav from "./components/Nav";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Experience from "./pages/Experience";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div className="app">
       <Nav />
       <Routes>
-        <Route path="/" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/experience" element={<Experience />} />
       </Routes>
