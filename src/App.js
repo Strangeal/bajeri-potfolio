cb6b5083dfe726cf6c240bb87918cf68ef335627
strangeal <bajerijustice@gmail.com>
2023-03-12 11:06:00 -0700

    Define routes for pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div className="App">hello world</div>;
-}
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Experience from "./pages/Experience";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/experience" element={<Experience />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
